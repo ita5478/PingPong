07824c0fdb653c579d5506d9746393d37a54c207
ita5478 <ittai.paz@gmail.com>
2022-01-19 17:21:49 +0200

    Addition: Add IReader interface.

---
+++ PingPongExercise/Client.BL.Abstraction/IO/IReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.BL.Abstraction.IO
+{
+    public interface IReader<out T>
+    {
+        T Read();
+    }
+}

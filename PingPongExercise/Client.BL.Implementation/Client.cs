0f73e77bf610d1eada8ab130fe437ce24b5d93b3
ita5478 <ittai.paz@gmail.com>
2022-01-19 17:27:55 +0200

    Implementation: Add Client that implements ClientBase.

---
+++ PingPongExercise/Client.BL.Implementation/Client.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Client.BL.Abstraction;
+
+namespace Client.BL.Implementation
+{
+    class Client : ClientBase
+    {
+        private Socket _socket;
+
+        
+
+        public override bool Connect()
+        {
+            _socket.Connect()
+        }
+
+        public override void Disconnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override byte[] ReadData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SendData(byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

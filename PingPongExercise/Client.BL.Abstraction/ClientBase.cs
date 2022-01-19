56801b536cdb16f42ac883086db96b9cab3f91d5
ita5478 <ittai.paz@gmail.com>
2022-01-19 17:15:41 +0200

    Addition: Add ClientBase abstract class.

---
+++ PingPongExercise/Client.BL.Abstraction/ClientBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Client.BL.Abstraction
+{
+    public abstract class ClientBase
+    {
+        public bool IsConnected { get; private set; }
+
+        public abstract Task<bool> Connect();
+
+        public abstract Task Disconnect();
+
+        public abstract Task SendData(byte[] data);
+
+        public abstract Task<byte[]> ReadData();
+
+    }
+}

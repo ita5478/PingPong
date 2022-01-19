e02c60ebec9d72661416b143d701d96e6994b90f
ita5478 <ittai.paz@gmail.com>
2022-01-19 17:21:06 +0200

    Addition: Add IWriter interface.

---
+++ PingPongExercise/Client.BL.Abstraction/IO/IWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.BL.Abstraction.IO
+{
+    public interface IWriter <in T>
+    {
+        void Write(T data);
+    }
+}

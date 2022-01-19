76783dd4ecaa7542eec914c54c3ba43f8124325a
ita5478 <ittai.paz@gmail.com>
2022-01-19 16:49:30 +0200

    Implementation: Add ClientHandler that implements IClientHandler.

---
+++ PingPongExercise/EchoServer.BL.Implementation/ClientHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using EchoServer.BL.Abstraction;
+using Common.Abstractions;
+
+namespace EchoServer.BL.Implementation
+{
+    public class ClientHandler : IClientHandler
+    {
+        private IStreamReaderAsync<object> _clientStreamReader;
+        private IStreamWriterAsync<object> _clientStreamWriter;
+
+        public async Task HandleClient()
+        {
+            var data = await _clientStreamReader.ReadAsync();
+            await _clientStreamWriter.Write(data);
+        }
+    }
+}

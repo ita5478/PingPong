38243e2821f528f343a19cb6f0847d94aa2b982f
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:57:13 +0200

    Modification: Modify ClientHandlerFactory to have a writer object.

--- PingPongExercise/EchoServer/EchoServer.BL.Implementation/ClientHandlerFactory.cs
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/ClientHandlerFactory.cs
@@ -1,13 +1,21 @@
 ﻿using EchoServer.BL.Abstraction;
 using EchoServer.BL.Abstraction.SocketWrappers;
+using EchoServer.BL.Abstraction.IO;
 
 namespace EchoServer.BL.Implementation
 {
     public class ClientHandlerFactory : IClientHandlerFactory
     {
+        private IWriter<string> _writer;
+
+        public ClientHandlerFactory(IWriter<string> writer)
+        {
+            _writer = writer;
+        }
+
         public IClientHandler Create(ISocketStreamWrapper socketWrapper)
         {
-            return new ClientHandler(socketWrapper);
+            return new ClientHandler(socketWrapper, _writer);
         }
     }
 }

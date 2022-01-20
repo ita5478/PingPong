ef117ebbfbd948ac02fcaa726f350ea821ea36d3
ita5478 <ittai.paz@gmail.com>
2022-01-20 15:50:13 +0200

    Implementation: Implement TcpSocketClientListenet.

---
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/TcpSocketImplementation/TcpSocketClientListener.cs
@@ -0,0 +1,38 @@
+﻿using EchoServer.BL.Abstraction;
+using EchoServer.BL.Abstraction.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer.BL.Implementation
+{
+    public class TcpSocketClientListener : ClientListenerBase
+    {
+        private IWriter<string> _writer;
+        private TcpListener _listener;
+
+        public TcpSocketClientListener(IClientHandlerFactory factory, IWriter<string> writer) 
+            : base(factory)
+        {
+            _writer = writer;
+        }
+
+        public override void Bind(int port)
+        {
+            _listener = new TcpListener(IPAddress.Parse("0.0.0.0"), port);
+        }
+
+        public override void ListenForClients()
+        {
+            _listener.Start();
+            System.Threading.CancellationTokenSource source = new System.Threading.CancellationTokenSource();
+
+            while (true)
+            {
+                var clientStream = new TcpSocketStreamWrapper(_listener.AcceptTcpClient().GetStream());
+                var handler = ClientHandlerFactory.Create(clientStream);
+                handler.HandleClient(source.Token);
+            }
+
+        }
+    }
+}

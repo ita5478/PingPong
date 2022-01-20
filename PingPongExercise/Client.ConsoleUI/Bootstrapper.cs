28a0b6506bf4ced5eda812ea7e3ec72a703e4bc8
ita5478 <ittai.paz@gmail.com>
2022-01-20 11:31:20 +0200

    Addition: Add bootstrapper.

---
+++ PingPongExercise/Client.ConsoleUI/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Client.BL.Implementation;
+
+namespace Client.ConsoleUI
+{
+    public class Bootstrapper
+    {
+        public ClientRunner Initialize()
+        {
+            var writer = new IO.ConsoleWriter();
+            var reader = new IO.ConsoleReader();
+
+            var socket = new ClientSocket();
+
+            ClientRunner runner = new ClientRunner(socket, writer, reader);
+            return runner;
+        }
+    }
+}

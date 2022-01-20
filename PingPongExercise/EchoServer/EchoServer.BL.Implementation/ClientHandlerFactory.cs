077f4293a4fe265ff8b8dd1ddda6680f5e9b6958
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:37:05 +0200

    Implementation: Implement ClientHandlerFactory.

---
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/ClientHandlerFactory.cs
@@ -0,0 +1,13 @@
+﻿using EchoServer.BL.Abstraction;
+using EchoServer.BL.Abstraction.SocketWrappers;
+
+namespace EchoServer.BL.Implementation
+{
+    public class ClientHandlerFactory : IClientHandlerFactory
+    {
+        public IClientHandler Create(ISocketStreamWrapper socketWrapper)
+        {
+            return new ClientHandler(socketWrapper);
+        }
+    }
+}

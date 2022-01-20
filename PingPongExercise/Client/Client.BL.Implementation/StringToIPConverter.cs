577bc581f61891be45fea41969fa88b8edca13d1
ita5478 <ittai.paz@gmail.com>
2022-01-20 21:30:06 +0200

    Addition: Add StringToIPConverter.

---
+++ PingPongExercise/Client/Client.BL.Implementation/StringToIPConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Client.BL.Abstraction;
+
+namespace Client.BL.Implementation
+{
+    public class StringToIPConverter : IConverter<string, IPAddress>
+    {
+        public string ConvertFrom(IPAddress input)
+        {
+            return input.ToString();
+        }
+
+        public IPAddress ConvertTo(string input)
+        {
+            return IPAddress.Parse(input);
+        }
+    }
+}

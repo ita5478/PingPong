867681b3fce3cddb7029215fa79a99a2358737c7
ita5478 <ittai.paz@gmail.com>
2022-01-20 22:29:34 +0200

    Implementation: Implement int parser.

---
+++ PingPongExercise/Common/Implementations/Parsers/IntParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Common.Abstractions.IO;
+
+namespace Common.Implementations.Parsers
+{
+    public class IntParser : IParser<int>
+    {
+        public int Parse(string data)
+        {
+            return int.Parse(data);
+        }
+    }
+}

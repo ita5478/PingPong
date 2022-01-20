0c4c19fa2fd7dfc5af3cd0c1c4d4f7d78de0ac7e
ita5478 <ittai.paz@gmail.com>
2022-01-20 11:20:43 +0200

    Implementation: Add ConsoleReader that implements IReader.

---
+++ PingPongExercise/Client.ConsoleUI/IO/ConsoleReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Client.BL.Abstraction.IO;
+
+namespace Client.ConsoleUI.IO
+{
+    public class ConsoleReader : IReader<string>
+    {
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+}

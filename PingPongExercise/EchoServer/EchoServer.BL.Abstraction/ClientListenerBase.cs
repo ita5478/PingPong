4d440ec939fb248145c8600ecaccfad732dbf0ce
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:33:59 +0200

    Modification: Modify IClientListener to be an abstract class.

---
+++ PingPongExercise/EchoServer/EchoServer.BL.Abstraction/ClientListenerBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+
+namespace EchoServer.BL.Abstraction
+{
+    public abstract class ClientListenerBase
+    {
+        protected IClientHandlerFactory ClientHandlerFactory;
+
+        public ClientListenerBase(IClientHandlerFactory factory)
+        {
+            ClientHandlerFactory = factory;
+        }
+
+        public abstract void Bind(int port);
+
+        public abstract void ListenForClients();
+    }
+}

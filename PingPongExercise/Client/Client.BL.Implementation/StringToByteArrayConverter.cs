08035a710cdbafc05a56fe737fb8f63e6a0bc1b5
ita5478 <ittai.paz@gmail.com>
2022-01-20 10:04:30 +0200

    Implementation: Implement IConverter in StringToByteArrayConverter.

---
+++ PingPongExercise/Client/Client.BL.Implementation/StringToByteArrayConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Client.BL.Abstraction;
+
+namespace Client.BL.Implementation
+{
+    public class StringToByteArrayConverter : IConverter<string, byte[]>
+    {
+        public byte[] Convert(string input)
+        {
+            return Encoding.ASCII.GetBytes(input);
+        }
+    }
+}

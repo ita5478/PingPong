4e6b3336046105ae80c02776c8725bb25c480ab6
ita5478 <ittai.paz@gmail.com>
2022-01-20 11:01:57 +0200

    Modification: Modify client.

--- PingPongExercise/Client/Client.BL.Implementation/Client.cs
+++ PingPongExercise/Client/Client.BL.Implementation/Client.cs
@@ -1,6 +1,8 @@
 ﻿using Client.BL.Abstraction.IO;
 using Client.BL.Abstraction;
 using System.Net;
+using System.Net.Sockets;
+using System;
 
 namespace Client.BL
 {
@@ -9,6 +11,7 @@ namespace Client.BL
         protected ClientSocketBase ClientSocket;
         protected IWriter<string> Writer;
         protected IReader<string> Reader;
+        protected IConverter<string, byte[]> Converter;
 
         public Client (ClientSocketBase socket, 
             IWriter<string> writer, 
@@ -23,17 +26,50 @@ namespace Client.BL
         {
             try
             {
-                IPAddress address = IPAddress.Parse(ip);
-                ClientSocket.Connect(address, port);
-                Writer.Write($"Connected Succussfully to {ip}:{port}");
+                if(!Connect(ip, port))
+                {
+                    return;
+                }
 
                 while (ClientSocket.IsConnected)
                 {
-                    Writer.Write("Enter the input you want to send to the server.");
                     string input = Reader.Read();
 
-                    ClientSocket.SendData()
+                    ClientSocket.SendData(Converter.ConvertTo(input));
+                    var data = ClientSocket.ReadData(input.Length);
+
+                    Writer.Write(Converter.ConvertFrom(data));
                 }
+            }
+            catch (SocketException)
+            {
+                Writer.Write("There's been an error with the connection to the server.");
+            }
+        }
+
+        private bool Connect(string ip, int port)
+        {
+            try
+            {
+                IPAddress address = IPAddress.Parse(ip);
+                ClientSocket.Connect(address, port);
+                Writer.Write($"Connected Succussfully to {ip}:{port}");
+                return true;
+            }
+            catch (FormatException)
+            {
+                Writer.Write("Invalid IP format entered.");
+                return false;
+            }
+            catch (ArgumentNullException)
+            {
+                Writer.Write("No IP entered.");
+                return false;
+            }
+            catch (SocketException)
+            {
+                Writer.Write($"Connection to {ip}:{port} failed.");
+                return false;
             }
         }
     }

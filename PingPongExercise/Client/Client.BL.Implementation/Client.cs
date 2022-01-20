20881feca0428fd066b2b7cd2c23f236e31013db
ita5478 <ittai.paz@gmail.com>
2022-01-20 09:31:11 +0200

    Modification: Modify Client class.

--- PingPongExercise/Client/Client.BL.Implementation/Client.cs
+++ PingPongExercise/Client/Client.BL.Implementation/Client.cs
@@ -1,4 +1,6 @@
 ﻿using Client.BL.Abstraction.IO;
+using Client.BL.Abstraction;
+using System.Net;
 
 namespace Client.BL
 {
@@ -8,13 +10,31 @@ namespace Client.BL
         protected IWriter<string> Writer;
         protected IReader<string> Reader;
 
-        public Client (ClientSocketBase socket, IWriter<string> writer, IReader<string> reader)
+        public Client (ClientSocketBase socket, 
+            IWriter<string> writer, 
+            IReader<string> reader)
 	    {
             ClientSocket = socket;
             Writer = writer;
             Reader = reader;
 	    }
 
+        public void Start(string ip, int port)
+        {
+            try
+            {
+                IPAddress address = IPAddress.Parse(ip);
+                ClientSocket.Connect(address, port);
+                Writer.Write($"Connected Succussfully to {ip}:{port}");
 
+                while (ClientSocket.IsConnected)
+                {
+                    Writer.Write("Enter the input you want to send to the server.");
+                    string input = Reader.Read();
+
+                    ClientSocket.SendData()
+                }
+            }
+        }
     }
 }

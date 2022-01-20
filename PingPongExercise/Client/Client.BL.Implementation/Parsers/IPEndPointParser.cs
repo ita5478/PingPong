20fed5e9ffde0de1c5348fce989a4202288dd4ae
ita5478 <ittai.paz@gmail.com>
2022-01-20 22:34:54 +0200

    Addition: Add IPEndPointParser.

---
+++ PingPongExercise/Client/Client.BL.Implementation/Parsers/IPEndPointParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Common.Abstractions.IO;
+using Common.Implementations.Parsers;
+
+namespace Client.BL.Implementation.Parsers
+{
+    public class IPEndPointParser : IParser<IPEndPoint>
+    {
+        private IntParser intParser;
+        private IpParser ipParser;
+
+        public IPEndPointParser()
+        {
+            intParser = new IntParser();
+            ipParser = new IpParser();
+        }
+
+        public IPEndPoint Parse(string data)
+        {
+            string[] parts = data.Split(':');
+
+            var ip = ipParser.Parse(parts[0]);
+            var port = intParser.Parse(parts[1]);
+
+            return new IPEndPoint(ip, port);
+        }
+    }
+}

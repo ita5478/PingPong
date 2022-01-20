b3d13570a43686d09329285c63b5a1ed923a8c63
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:57:45 +0200

    Modification: Modify SocketClientListener to have a writer object.

--- PingPongExercise/EchoServer/EchoServer.BL.Implementation/SocketClientListener.cs
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/SocketClientListener.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using EchoServer.BL.Abstraction;
+using EchoServer.BL.Abstraction.IO;
 
 namespace EchoServer.BL.Implementation
 {
@@ -10,16 +11,21 @@ namespace EchoServer.BL.Implementation
     {
 
         private Socket _listener;
+        private IWriter<string> _writer;
 
-        public SocketClientListener(IClientHandlerFactory factory) : base(factory)
+        public SocketClientListener(IClientHandlerFactory factory, IWriter<string> writer) 
+            : base(factory)
         {
             _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _writer = writer;
         }
 
         public override void Bind(int port)
         {
             _listener.Bind(new IPEndPoint(IPAddress.Parse("0.0.0.0"), port));
             _listener.Listen(100);
+
+            _writer.Write($"Listening on port {port}.");
         }
 
         public override void ListenForClients()
@@ -27,7 +33,7 @@ namespace EchoServer.BL.Implementation
             while (true)
             {
                 var socketStream = new SocketStreamWrapper(_listener.Accept());
-
+                _writer.Write("A new connection has been made.");
                 var handler = ClientHandlerFactory.Create(socketStream);
                 Task.Run(handler.HandleClient);
             }

27829ce224d6f16b56495decbf26e4328fa221a5
ita5478 <ittai.paz@gmail.com>
2022-01-20 22:27:49 +0200

    Implementation: Implement IP parser.

---
+++ PingPongExercise/Common/Implementations/Parsers/IpParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Common.Abstractions.IO;
+
+namespace Common.Implementations.Parsers
+{
+    public class IpParser : IParser<IPAddress>
+    {
+        public IPAddress Parse(string data)
+        {
+            return IPAddress.Parse(data);
+        }
+    }
+}

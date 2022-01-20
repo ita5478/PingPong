8964c8a742ec34b04ada4860ac71bf772aaf3703
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:44:04 +0200

    Modification: Modify main to start the program.

--- PingPongExercise/EchoServer.Startup/Program.cs
+++ PingPongExercise/EchoServer.Startup/Program.cs
@@ -4,9 +4,14 @@ namespace EchoServer.Startup
 {
     class Program
     {
+        const int PORT = 6666;
         static void Main(string[] args)
         {
-            
+            Bootstrapper boot = new Bootstrapper();
+            var listener = boot.Initialize();
+
+            listener.Bind(PORT);
+            listener.ListenForClients();
         }
     }
 }

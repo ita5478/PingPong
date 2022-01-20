8aa76e2a2c80009b6c07c85c52a0f38d7f955685
ita5478 <ittai.paz@gmail.com>
2022-01-20 11:21:46 +0200

    Implementation: Add ConsoleWriter that implements IWriter.

---
+++ PingPongExercise/Client.ConsoleUI/IO/ConsoleWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Client.BL.Abstraction.IO;
+
+namespace Client.ConsoleUI.IO
+{
+    public class ConsoleWriter : IWriter<string>
+    {
+        public void Write(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}

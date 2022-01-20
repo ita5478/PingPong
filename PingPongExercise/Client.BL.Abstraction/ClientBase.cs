6d2511e2632018a181e01dc3941d8ba4f98809c7
ita5478 <ittai.paz@gmail.com>
2022-01-20 08:25:27 +0200

    Addition: Add ClientBase abstraction. Not finished.

---
+++ PingPongExercise/Client.BL.Abstraction/ClientBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.BL.Abstraction
+{
+    public abstract class ClientBase
+    {
+        protected ClientSocketBase ClientSocket;
+         
+
+        public ClientBase(ClientSocketBase socket)
+        {
+            ClientSocket = socket;
+        }
+
+
+    }
+}

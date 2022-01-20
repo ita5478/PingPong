664e6071a319349978e09966fc5fd6de6f9fee87
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:07:54 +0200

    Implementation: Add SocketStreamWrapper that implements ISocketStreamWrapper.

---
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/SocketStreamWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using EchoServer.BL.Abstraction.SocketWrappers;
+
+
+namespace EchoServer.BL.Implementation
+{
+    public class SocketStreamWrapper : ISocketStreamWrapper
+    {
+        private const int INTERVAL = 100;
+        private Socket _socket;
+
+        public SocketStreamWrapper(Socket socket)
+        {
+            _socket = socket;
+        }
+
+        public void Close()
+        {
+            _socket.Close();
+        }
+
+        public async Task<byte[]> ReadAsync(int bufferSize)
+        {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.SetBuffer(new byte[bufferSize], 0, bufferSize);
+            while (_socket.ReceiveAsync(args))
+            {
+                await Task.Delay(INTERVAL);
+            }
+
+            return args.Buffer;
+        }
+
+        public async Task WriteAsync(byte[] buffer)
+        {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.SetBuffer(buffer);
+
+            while (_socket.SendAsync(args))
+            {
+                await Task.Delay(INTERVAL);
+            }
+        }
+    }
+}

07afe3cd431d3eb8593cd229591e9cad7678c636
ita5478 <ittai.paz@gmail.com>
2022-01-20 13:56:50 +0200

    Modification: Modify ClientHandler to have a writer object.

--- PingPongExercise/EchoServer/EchoServer.BL.Implementation/ClientHandler.cs
+++ PingPongExercise/EchoServer/EchoServer.BL.Implementation/ClientHandler.cs
@@ -3,16 +3,18 @@ using System.Net;
 using System.Net.Sockets;
 using EchoServer.BL.Abstraction;
 using EchoServer.BL.Abstraction.SocketWrappers;
+using EchoServer.BL.Abstraction.IO;
 
 namespace EchoServer.BL.Implementation
 {
     public class ClientHandler : IClientHandler
     {
         private ISocketStreamWrapper _socketStream;
-
-        public ClientHandler(ISocketStreamWrapper stream)
+        private IWriter<string> _writer;
+        public ClientHandler(ISocketStreamWrapper stream, IWriter<string> writer)
         {
             _socketStream = stream;
+            _writer = writer;
         }
 
         public async Task HandleClient()
@@ -20,10 +22,13 @@ namespace EchoServer.BL.Implementation
             try
             {
                 var data = await _socketStream.ReadAsync(1000);
+                _writer.Write("Received bytes. Sending them back to the client.");
                 await _socketStream.WriteAsync(data);
+                _writer.Write("Succussfuly sent the data back to the client.");
             }
             catch (SocketException)
             {
+                _writer.Write("Socket isn't responding. Closing connection.");
                 _socketStream.Close();
             }
         }

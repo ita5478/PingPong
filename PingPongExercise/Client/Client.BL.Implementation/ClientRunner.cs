e76d8b0f5e0db5b338c25b08f1a89e4ebf051f4f
ita5478 <ittai.paz@gmail.com>
2022-01-20 21:27:46 +0200

    Modification: Modify ClientRunner to only execute the connection and the client action flow.

--- PingPongExercise/Client/Client.BL.Implementation/ClientRunner.cs
+++ PingPongExercise/Client/Client.BL.Implementation/ClientRunner.cs
@@ -8,52 +8,28 @@ namespace Client.BL.Implementation
 {
     public class ClientRunner
     {
-        protected ClientSocketBase ClientSocket;
-        protected IWriter<string> Writer;
-        protected IReader<string> Reader;
-        protected IConverter<string, byte[]> StringToByteConverter;
         protected IConverter<string, IPAddress> StringToIPConverter;
         protected IConnectionInitializer ConnectionInitializer;
+        protected IClientAction ClientAction;
 
-        public ClientRunner(ClientSocketBase socket,
-            IWriter<string> writer,
-            IReader<string> reader,
-            IConverter<string, byte[]> stringToByteConverter,
-            IConverter<string, IPAddress> stringToIPConverter,
-            IConnectionInitializer connectionInitializer)
+        public ClientRunner(
+            IConnectionInitializer connectionInitializer,
+            IClientAction clientAction,
+            IConverter<string, IPAddress> stringToIPConverter)
         {
-            ClientSocket = socket;
-            Writer = writer;
-            Reader = reader;
-            StringToByteConverter = stringToByteConverter;
+            ClientAction = clientAction;
             StringToIPConverter = stringToIPConverter;
             ConnectionInitializer = connectionInitializer;
         }
 
         public void Start(string ip, int port)
         {
-            try
+            if (!ConnectionInitializer.Connect(StringToIPConverter.ConvertTo(ip), port))
             {
-                if (!ConnectionInitializer.Connect(StringToIPConverter.ConvertTo(ip), port))
-                {
-                    return;
-                }
-
-                while (ClientSocket.IsConnected)
-                {
-                    string input = Reader.Read();
-
-                    ClientSocket.SendData(StringToByteConverter.ConvertTo(input));
-                    Writer.Write($"Sent {input} to the server.");
-                    var data = ClientSocket.ReadData(input.Length);
-
-                    Writer.Write(StringToByteConverter.ConvertFrom(data));
-                }
+                return;
             }
-            catch (SocketException)
-            {
-                Writer.Write("There's been an error with the connection to the server.");
-            }
+
+            ClientAction.Execute();
         }
     }
 }

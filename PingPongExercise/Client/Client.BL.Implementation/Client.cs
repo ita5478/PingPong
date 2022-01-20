f22bd54b802ee9407193ee90196c12baab177ff2
ita5478 <ittai.paz@gmail.com>
2022-01-20 08:44:31 +0200

    Modification: Modify ClientBase to be Client in implementation.

---
+++ PingPongExercise/Client/Client.BL.Implementation/Client.cs
@@ -0,0 +1,20 @@
+﻿using Client.BL.Abstraction.IO;
+
+namespace Client.BL
+{
+    public class Client
+    {
+        protected ClientSocketBase ClientSocket;
+        protected IWriter<string> Writer;
+        protected IReader<string> Reader;
+
+        public Client (ClientSocketBase socket, IWriter<string> writer, IReader<string> reader)
+	    {
+            ClientSocket = socket;
+            Writer = writer;
+            Reader = reader;
+	    }
+
+
+    }
+}

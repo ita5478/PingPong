0125f57960104c11a16bf822653ff0529ae6f0a5
ita5478 <ittai.paz@gmail.com>
2022-01-21 14:21:10 +0200

    SolutionChange: Seperate Client.Core from EchoClient.

---
+++ PingPongExercise/EchoClient.BL.Implementations/ClientActions/SendMessageToEchoServerAction.cs
@@ -0,0 +1,50 @@
+﻿using Client.BL.Core.Abstraction;
+using Common.Abstractions.IO;
+using System.Net.Sockets;
+
+namespace Client.BL.Implementation
+{
+    public class SendMessageToEchoServerAction : IClientAction
+    {
+        private ClientSocketBase _clientSocket;
+        private IWriter<string> _userOutputWriter;
+        private IReader<string> _userInputReader;
+        public IConverter<string, byte[]> _stringToByteConverter;
+
+        public SendMessageToEchoServerAction(
+            ClientSocketBase clientSocket,
+            IWriter<string> userOutputWriter,
+            IReader<string> userInputReader,
+            IConverter<string, byte[]> stringToByteConverter)
+        {
+            _clientSocket = clientSocket;
+            _userOutputWriter = userOutputWriter;
+            _userInputReader = userInputReader;
+            _stringToByteConverter = stringToByteConverter;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                while (_clientSocket.IsConnected)
+                {
+                    _userOutputWriter.Write("Enter a message you want to send to the server:");
+                    string input = _userInputReader.Read();
+
+                    _clientSocket.SendData(_stringToByteConverter.ConvertTo(input));
+                    _userOutputWriter.Write($"Sent {input} to the server.");
+
+                    var data = _clientSocket.ReadData(input.Length);
+                    var receivedData = _stringToByteConverter.ConvertFrom(data);
+                    _userOutputWriter.Write($"The server returned {receivedData}.");
+                }
+
+            }
+            catch (SocketException)
+            {
+                _userOutputWriter.Write("There's been an error with the connection to the server.");
+            }
+        }
+    }
+}

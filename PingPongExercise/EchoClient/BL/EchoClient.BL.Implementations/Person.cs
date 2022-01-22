23a14da77b06a63fb03b00fa87152d474d01c514
ita5478 <ittai.paz@gmail.com>
2022-01-22 10:48:07 +0200

    Addition: Add Person class.

---
+++ PingPongExercise/EchoClient/BL/EchoClient.BL.Implementations/Person.cs
@@ -0,0 +1,21 @@
+﻿
+namespace EchoClient.BL.Implementations
+{
+    [System.Serializable]
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} is {_age} years old.";
+        }
+    }
+}
